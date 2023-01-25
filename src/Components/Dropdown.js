3979a6e6890a27483272cf98a557701317d17096
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-01-25 08:29:15 +0000

    mapping

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { serviceDropdown } from './NavItems'
+import { Link } from 'react-router-dom'
+import "./Dropdown.css"
 
 const Dropdown = () => {
   return (
     <>
-    
+    <ul>
+        {serviceDropdown.map(item => {
+            return(
+                <li key={item.id}>
+                    <Link className={item.cName}>{item.title}</Link>
+                </li>
+            )
+        })}
+    </ul>
     </>
   )
 }
